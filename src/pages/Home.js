2cfbde63acd9d31a0efd0301aa64caaf43049437
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2022-09-29 11:11:20 +0530

    Homepage Is Added

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return(
+    <div className="mb-20">
+        <h1 className="sm:text-4xl text-2xl font-bold mt-6 to-gray-900">Welcome to My Blog!</h1>
+
+        <p className="mx-auto leading-relaxed text-base mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis beatae similique labore quae. Rem eaque vero molestias magnam animi odio sunt exercitationem iste consectetur voluptate similique eligendi, facilis explicabo veritatis.</p>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis beatae similique labore quae. Rem eaque vero molestias magnam animi odio sunt exercitationem iste consectetur voluptate similique eligendi, facilis explicabo veritatis.</p>
+    </div>
+    );
+};
+
+export default Home;
